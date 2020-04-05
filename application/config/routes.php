566aaf374e31b9bac70a64ea6c1bd6f6c97b16de
kendall <kendall.shearman@g.austincc.edu>
2020-04-05 13:42:28 -0500

    /* Adding previously unversioned file views/users/custom404.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,9 +50,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 
-//$route['users/register'] = 'users/register';
-$route['users/signin'] = 'users/signin';
-$route['users'] = 'users/index';
+
+/*$route['users/signin'] = 'users/signin';
+//$route['users'] = 'users/index';
+$route['users'] = 'users/$1';
 $route['users/(:any)'] = 'users/index/$1';
+//$route['users/register'] = 'users/register';
+$route['default_controller'] = 'pages/view';
+$route['(:any)'] = 'pages/view/$1';*/
+
+
+
+$route['users'] = 'users/index';
+$route['users'] = 'users/$1';
+$route['users/(:any)'] = 'users/index/$1';
+$route['users/register'] = 'users/register';
+$route['users/signin'] = 'users/signin';
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
+
